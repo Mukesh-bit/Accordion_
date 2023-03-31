176b7970ef728777f8f502cf7f2d27c7cbbc1148
Mukesh Kumar <75024929+Mukesh-bit@users.noreply.github.com>
2023-03-31 14:01:52 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const ourItem = document.getElementsByClassName("item");
+const openIcon = document.getElementsByClassName("iconOpen");
+const closeIcon = document.getElementsByClassName("iconClose");
+
+
+
+for(let i=0; i<ourItem.length; i++){
+    closeIcon[i].style.display = "none";
+
+    ourItem[i].addEventListener("click", () =>{
+        const result = ourItem[i].classList.toggle("active");
+
+        if(result){
+            closeIcon[i].style.display = "block";
+            openIcon[i].style.display = "none";
+        }else{
+            closeIcon[i].style.display = "none";
+            openIcon[i].style.display = "block";
+        }
+    })
+}
